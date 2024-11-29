1e9cd89a9d6b539b766f5b1f8f4d554ce2b6a420
jiyoung <aa991102@naver.com>
2024-11-29 09:12:18 +0900

    vite 설정

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { getNode, insertLast } from "kind-tiger";
+import { insertLast } from "./../node_modules/kind-tiger/dom/insert";
+
+const app = getNode("#app");
+
+const template = ``;
+
+insertLast(app, template);
