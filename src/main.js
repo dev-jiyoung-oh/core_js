e323395103f566520fa4fb2376c8b5b7b3a12731
jiyoung <aa991102@naver.com>
2024-11-29 10:26:42 +0900

    vite 자동설치

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { getNode, insertLast } from "kind-tiger";
-import { insertLast } from "./../node_modules/kind-tiger/dom/insert";
-
-const app = getNode("#app");
-
-const template = ``;
-
-insertLast(app, template);
